2ca38e271eb58c2f19c2b92528cc9995b72d932c
Evan Liegey <evan.liegey@etudiant.univ-reims.fr>
2026-02-02 19:01:57 +0100

    Remise en forme de l'architecture + page d'accueil

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Club Bultaco - Escuyer</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <h1>Club Bultaco</h1>
+    <img src="../img/logo_rond.png" alt="logo">
+</body>
+</html>
---
+++ public/restauration.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="../img/logo.png" alt="logo">
+        <ul>
+            <li>Accueil</li>
+            <li>Restauration</li>
+            <li>Pièces détachées</li>
+            <li>Emblèmes</li>
+        </ul>
+    </header>
+</body>
+</html>
